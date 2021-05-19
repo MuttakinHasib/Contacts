f1a8317c57b4c7af011aab4b2d87870a5070c8c7
MuttakinHasib <hasibmolla28@gmail.com>
2021-05-19 22:51:01 +0600

    :bug:

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import { useIsDrawerOpen } from "@react-navigation/drawer";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Platform } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 import { tailwind } from "../../lib/tailwind";
-import { statusbarHeight } from "../utils/statusbar";
 import RoundedIcon from "./RoundedIcon";
 
 const Header = ({ label, left, right }) => {
@@ -20,16 +20,15 @@ const Header = ({ label, left, right }) => {
       setBarStyle("dark");
     }
   }, [isDrawerOpen]);
-  // const SIZE = 58.17 + statusbarHeight || insets.top;
-  const SIZE = insets.top * 3;
-  console.log(statusbarHeight, insets.top);
+
+  const SIZE = insets.top * 3 + (Platform.OS === "ios" && insets.top);
+
   return (
     <View
       style={[
         tailwind("bg-white flex-row justify-between items-center px-5"),
         {
           paddingTop: insets.top,
-          // paddingVertical: insets.top,
           height: SIZE,
         },
       ]}
