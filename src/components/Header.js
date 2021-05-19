c9c8b413bfd1c66ff6de9f3af39535c4d3648f43
MuttakinHasib <hasibmolla28@gmail.com>
2021-05-19 22:33:13 +0600

    :bug:

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,14 +21,14 @@ const Header = ({ label, left, right }) => {
     }
   }, [isDrawerOpen]);
   // const SIZE = 58.17 + statusbarHeight || insets.top;
-  const SIZE = statusbarHeight * 3 || insets.top * 3;
+  const SIZE = insets.top * 3;
   console.log(statusbarHeight, insets.top);
   return (
     <View
       style={[
         tailwind("bg-white flex-row justify-between items-center px-5"),
         {
-          paddingTop: statusbarHeight || insets.top,
+          paddingTop: insets.top,
           // paddingVertical: insets.top,
           height: SIZE,
         },
--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -4,7 +4,9 @@ export const login = () => async dispatch => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
     dispatch({ type: USER_LOGIN_SUCCESS, payload: true });
-  } catch (err) {}
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 export const logout = () => dispatch => {
