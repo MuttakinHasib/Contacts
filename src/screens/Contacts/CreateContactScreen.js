ec8e2ca518617bdd2e3feba37033805dea876657
MuttakinHasib <hasibmolla28@gmail.com>
2021-05-19 15:16:22 +0600

    add image picker :zap:

--- src/screens/Contacts/CreateContactScreen.js
+++ src/screens/Contacts/CreateContactScreen.js
@@ -1,13 +1,33 @@
 import { Avatar } from "@ui-kitten/components";
-import React from "react";
-import { View, Text, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Dimensions, TouchableOpacity } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import * as ImagePicker from "expo-image-picker";
+
 import { getColor, tailwind } from "../../../lib/tailwind";
 import { AppInput, Button, Header } from "../../components";
 
 const { height } = Dimensions.get("screen");
 
 const CreateContactScreen = ({ navigation }) => {
+  const [avatar, setAvatar] = useState(
+    "https://res.cloudinary.com/muttakinhasib/image/upload/v1621273993/avatar/user_dmy5bs.png"
+  );
+
+  const handlePickerAvatar = async () => {
+    await ImagePicker.getCameraPermissionsAsync();
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+    });
+
+    if (!result.cancelled) {
+      setAvatar(result.uri);
+    }
+  };
+
   return (
     <KeyboardAwareScrollView>
       <View style={{ height }}>
@@ -22,12 +42,14 @@ const CreateContactScreen = ({ navigation }) => {
           }}
         />
         <View style={tailwind("p-5")}>
-          <Avatar
-            style={tailwind("self-center w-20 h-20")}
-            source={{
-              uri: "https://res.cloudinary.com/muttakinhasib/image/upload/v1621273993/avatar/user_dmy5bs.png",
-            }}
-          />
+          <TouchableOpacity onPress={handlePickerAvatar}>
+            <Avatar
+              style={tailwind("self-center w-28 h-28")}
+              source={{
+                uri: avatar,
+              }}
+            />
+          </TouchableOpacity>
           <View>
             <AppInput label="Name" placeholder="Hasib Molla" />
             <AppInput
